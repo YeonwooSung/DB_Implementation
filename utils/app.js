d424ec0a2a5661cc6f86b924fe248700b4f09ffc
YeonwooSung <neos960518@gmail.com>
2018-11-02 10:27:52 +0900

    make the default router

--- utils/app.js
+++ utils/app.js
@@ -5,6 +5,12 @@ let path = require('path');
 
 let app = express()
 
+//The routers objects.
+let indexRouter = require('./routes/index');
+
+//Add router objects
+app.use('/', indexRouter);
+
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
     next(createError(404));
