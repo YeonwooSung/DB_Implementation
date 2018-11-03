8833cfa374f3d4b4f7e6e088f34c6c359c46f375
YeonwooSung <neos960518@gmail.com>
2018-11-04 08:42:44 +0900

    start implementing the db connection part

--- utils/dbConnection.js
+++ utils/dbConnection.js
@@ -6,9 +6,29 @@ const mysql = require('mysql');
  * @type {Pool} the database pool.
  */
 let pool = mysql.createPool({
-    connectionLimit: 1000,
+    connectionLimit: 100,
     host: 'localhost',
     user: '',
     password: '',
     database: ''
 });
+
+exports.listAllCostomers = () => {
+    const queryString = `SELECT id, name, email, numOfBooks, totalSpentMoney FROM User`;
+
+    pool.getConnection(function(err, conn) {
+        if (err) {
+            //TODO on error
+        } else {
+            conn.query(queryString, function(err, result, fields) {
+                if (err) {
+                    //TODO on error
+                } else {
+                    //TODO on success
+                }
+            });
+
+            conn.release(); //release the connection
+        }
+    });
+}
