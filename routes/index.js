432869fcd0d78899dbd818147629aa491217699a
YeonwooSung <neos960518@gmail.com>
2018-11-04 09:11:20 +0900

    use the view engine to render the html

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,11 @@
+'use strict';
+
 const express = require('express');
 const router = express.Router();
 
 /* GET home page. */
 router.get('/', function (req, res) {
-    //res.render('index', { title: 'Data Thing' });
+    res.render('index', {userName: 'me'});
 });
 
 module.exports = router;
--- utils/app.js
+++ utils/app.js
@@ -1,12 +1,20 @@
+'use strict';
+
 //import required libraries
 let createError = require('http-errors');
 let express = require('express');
 let path = require('path');
+let consolidate = require('consolidate');
 
 let app = express()
 
 //The routers objects.
 let indexRouter = require('./routes/index');
+
+//Set the view engine
+app.engine('html', consolidate.swig);
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', html);
 
 //Add router objects
 app.use('/', indexRouter);
