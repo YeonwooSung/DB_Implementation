4c08b7ca8e4f3477ce0b436869db73d4a2318e8f
YeonwooSung <neos960518@gmail.com>
2018-11-07 10:56:59 +0900

    minimal improvement

--- utils/dbConnection.js
+++ utils/dbConnection.js
@@ -2,6 +2,8 @@
 
 const mysql = require('mysql');
 
+const Q1 = `SELECT id, name, email, numOfBooks, totalSpentMoney FROM User`;
+
 /**
  * This variable creates the db pool to control the db connection objects.
  *
@@ -16,7 +18,7 @@ let pool = mysql.createPool({
 });
 
 exports.listAllCostomers = () => {
-    const queryString = `SELECT id, name, email, numOfBooks, totalSpentMoney FROM User`;
+    const queryString = Q1;
 
     pool.getConnection(function(err, conn) {
         if (err) {
