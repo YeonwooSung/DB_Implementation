9c4882fe2d5e7767972e50d6330b8883f8b577b2
YeonwooSung <neos960518@gmail.com>
2018-11-08 20:29:54 +0900

    improve the public router

--- routes/publicRouter.js
+++ routes/publicRouter.js
@@ -8,4 +8,9 @@ router.get('/stylesheets/index.css', function (req, res) {
     res.sendFile(__dirname + '/../public/stylesheets/index.css');
 });
 
+/* GET index_page.js */
+router.get('/javascripts/index_page.js', function (req, res) {
+    res.sendFile(__dirname + '/../public/javascripts/index_page.js');
+});
+
 module.exports = router;
