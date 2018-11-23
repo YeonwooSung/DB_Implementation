60827e4b2f3c4691f38b446c9e6218392801ab48
YeonwooSung <neos960518@gmail.com>
2018-11-23 11:31:50 +0900

    add the pug module

--- utils/app.js
+++ utils/app.js
@@ -17,6 +17,7 @@ let indexRouter = require('../routes/index');
 let publicRouter = require('../routes/publicRouter');
 
 //Set the view engine
+app.engine('pug', require('pug').__express)
 app.set('views', path.join(__dirname, '../views'));
 app.set('view engine', 'pug'); //use the pug as a template engine
 
