4f817d2a53c13e9e41f7c5b5a4e84116a07eb94b
YeonwooSung <neos960518@gmail.com>
2018-11-24 06:50:41 +0900

    write sql files to insert sample data 1

---
+++ sql_files/create_tables/audiobooks.sql
@@ -0,0 +1,47 @@
+-- Insert sample data for audio book
+
+INSERT INTO audiobook SET 
+ISBN = '860-1404211171', 
+title = 'Fantastic Beasts and Where to Find Them',
+narrator_id = 3, running_time = '01:45:00', 
+purchase_price = '12.00', 
+publisher_name = 'Pottermore Publishing',
+published_date = '2017-03-14';
+
+INSERT INTO audiobook SET 
+ISBN = '978-0393957242',
+title = "Gulliver's Travels",
+narrator_id = 4,
+running_time = '05:35:00',
+purchase_price = '38.00',
+publisher_name = 'Penguin Books Ltd',
+published_date = '2012-05-12';
+
+INSERT INTO audiobook SET
+ISBN = '978-1408855652',
+title = "Harry Potter and the Philosopher's Stone",
+narrator_id = 3,
+running_time = '08:44:00',
+purchase_price = '7.19',
+publisher_name = 'Pottermore Publishing',
+published_date = '2014-09-01';
+
+INSERT INTO audiobook SET
+ISBN = '978-0099457046',
+title = 'Moab Is My Washpot',
+narrator_id = 3,
+running_time = '11:33:00',
+age_rating = '12',
+purchase_price = '22.00',
+publisher_name = 'Random House AudioBooks',
+published_date = '2017-01-01';
+
+INSERT INTO audiobook SET
+ISBN = '978-1611749731',
+title = 'The Gun Seller',
+narrator_id = 6,
+running_time = '10:45:00',
+age_rating = '16',
+purchase_price = '16.00',
+publisher_name = 'HighBridge Audio',
+published_date = '2012-10-16';
---
+++ sql_files/create_tables/authors.sql
@@ -0,0 +1,13 @@
+-- Insert the sample data for audio book authors
+
+INSERT INTO audiobook_authors SET contributor_ID = 7, ISBN = "860-1404211171";
+
+INSERT INTO audiobook_authors SET contributor_ID = 8, ISBN = "860-1404211171";
+
+INSERT INTO audiobook_authors SET contributor_ID = 11, ISBN = "978-0393957242";
+
+INSERT INTO audiobook_authors SET contributor_ID = 7, ISBN = "978-1408855652";
+
+INSERT INTO audiobook_authors SET contributor_ID = 3, ISBN = "978-0099457046";
+
+INSERT INTO audiobook_authors SET contributor_ID = 4, ISBN = "978-1611749731";
