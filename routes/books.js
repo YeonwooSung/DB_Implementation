beeb37f321be3650f6217f54a9864ef94e3866cd
YeonwooSung <neos960518@gmail.com>
2018-11-27 08:04:40 +0900

    almost done with the /books page

--- routes/books.js
+++ routes/books.js
@@ -4,10 +4,16 @@ const router = express.Router();
 //use the sql queries to check if the user is registered.
 const conn = require('../utils/dbConnection');
 
+let path = require('path');
+
 /* GET users listing. */
 router.get('/', function (req, res) {
     conn.listAllAudiobooks(res); //get all audiobooks from db and send the response to the client
 });
 
+/* GET books.css */
+router.get('/stylesheets/register.css', function (req, res) {
+    res.sendFile(path.resolve(__dirname + '/../public/stylesheets/books.css'));
+});
 
 module.exports = router;
