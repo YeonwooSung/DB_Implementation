b017341df32fb710ea7e080617013000b87572e3
YeonwooSung <neos960518@gmail.com>
2018-11-26 11:07:49 +0900

    drop all tables in the database

---
+++ sql_files/setUp_tables/dropTables.sql
@@ -0,0 +1,12 @@
+-- Drop all tables in the database.
+
+DROP Table audiobook_reviews;
+DROP Table audiobook_purchases;
+DROP Table audiobook_authors;
+DROP Table chapter;
+DROP Table audiobook;
+DROP table publisher;
+DROP table phone_number;
+DROP table customer;
+DROP table contributor;
+DROP table person;
