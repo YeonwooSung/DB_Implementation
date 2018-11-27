bd32e6ed496f570a2b8247831425f85749375b42
YeonwooSung <neos960518@gmail.com>
2018-11-27 12:10:41 +0900

    change type from datetime to date

--- sql_files/setUp_tables/createDB.sql
+++ sql_files/setUp_tables/createDB.sql
@@ -51,7 +51,7 @@ CREATE TABLE audiobook (
     age_rating INT,
     purchase_price decimal(8,2),
     publisher_name VARCHAR(1024),
-    published_date DATETIME,
+    published_date DATE,
     audiofile BLOB,
     primary key(ISBN),
     foreign key(narrator_id) references person(ID),
